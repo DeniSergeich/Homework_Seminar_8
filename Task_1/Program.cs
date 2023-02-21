32ade632d4e05ae9cb389c95b1d8462a25e57971
Denis <damned_wolf@mail.ru>
2023-02-21 23:57:49 +0300

    Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -50,12 +50,10 @@ void PrintArray(int[,] inArray)
 
 void SortArray(int[,] inArray)
 {
-
     for (int i = 0; i < array.GetLength(0); i++) 
     {
         for (int j = 0; j < array.GetLength(1); j++) 
-        {
-            
+        {            
             for (int k = 0; k < inArray.GetLength(1); k++)
             {
                 {
