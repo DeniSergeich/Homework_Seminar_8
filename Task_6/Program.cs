a645c1ac3cb01ac10a3a63ee42555c566411e1df
Denis <damned_wolf@mail.ru>
2023-02-23 14:14:16 +0300

    Task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,46 @@
+﻿
+Console.Clear();
+int row = Promt("Введите количество строк треугольника Паскаля: ");
+int[,] triangle = new int[row, row];
+const int cellWidth = 3;
+FillTriangle();
+MajicPrint();
+
+void FillTriangle()
+{
+    for (int i = 0; i < row; i++)
+    {
+        triangle[i, 0] = 1;
+        triangle[i, i] = 1;
+    }
+    for (int i = 2; i < row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            triangle[i, j] = triangle[i - 1, j - 1] + triangle[i - 1, j];
+        }
+    }
+}
+
+void MajicPrint()
+{
+    int col = cellWidth * row;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            Console.SetCursorPosition(col, i + 1);
+            if(triangle[i,j] != 0) Console.Write($"{triangle[i,j], cellWidth}");
+            col += cellWidth*2;
+        }
+        col = cellWidth*row-cellWidth*(i+1);
+        Console.WriteLine();
+    }
+}
+
+int Promt(string massage)
+{
+    Console.Write(massage);    
+    int value = int.Parse(Console.ReadLine());    
+    return value;
+}
